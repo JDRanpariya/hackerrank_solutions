8c5a2fc8f497992575b7e824ac0ae84e371c424c
JDRanpariya <jdpatel.code@gmail.com>
2020-03-16 16:28:38 +0530

    linux shell added

---
+++ Linux Shell/Bash/Looping.sh
@@ -0,0 +1,7 @@
+#Basic while loop syntex
+
+i=1
+for i in {1..50}
+do 
+    echo $i
+done
---
+++ Linux Shell/Bash/Personalised Echo.sh
@@ -0,0 +1,8 @@
+#take input by read
+#here varname can be any variable name
+
+read varname
+
+# $ calls the value of variable followed by
+
+echo "Welcome $varname"
